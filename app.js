7dd3c3dd67bc10da15ad943008b19920a643dae4
王远东 <847717352@qq.com>
2018-04-10 23:38:22 +0800

    整理代码，添加跨域访问

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const json = require('koa-json')
 const onerror = require('koa-onerror')
 const bodyparser = require('koa-bodyparser')
 const logger = require('koa-logger')
+const cors = require('koa2-cors')
 
 const index = require('./routes/index')
 const users = require('./routes/users')
@@ -16,6 +17,7 @@ onerror(app)
 app.use(bodyparser({
   enableTypes:['json', 'form', 'text']
 }))
+app.use(cors())
 app.use(json())
 app.use(logger())
 app.use(require('koa-static')(__dirname + '/public'))
