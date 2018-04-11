724b8da2cfa3e94577c8634db4be73a70c16b923
王远东 <847717352@qq.com>
2018-04-11 22:32:28 +0800

    测试跨域访问

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,10 @@
 const router = require('koa-router')()
 
-router.get('/', async (ctx, next) => {
+/*router.get('/', async (ctx, next) => {
   await ctx.render('index', {
     title: 'Hello Koa 2!'
   })
-})
+})*/
 
 router.get('/string', async (ctx, next) => {
   ctx.body = 'koa2 string from get'
